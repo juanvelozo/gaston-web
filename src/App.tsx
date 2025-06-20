4306aa8ba97818048c857411612e4b3fb17194e3
juanvelozo <juanvelozomoreno18@gmail.com>
2025-06-20 20:46:43 -0300

    actualiza comentarios, lista de TODO

--- src/App.tsx
+++ src/App.tsx
@@ -5,3 +5,29 @@ function App() {
 }
 
 export default App;
+
+/**
+ * - Consumir servicio de Resumen
+ * - Probar lógica de refresh token
+ * - Consumir crud de movimiento
+ * - Consumir crud de categoria
+ * - Integrar endpoints de perfil
+ * - Agregar assets para foto de perfil de usuario (Tal vez hacerla con IA?)
+ * - Integrar endpoints de cambiar contraseña
+ * - Hacer endpoint de recupero de contraseña
+ * - Analizar si hace falta redux o seguimos con Context
+ * - Buscar librería de componentes/estilos
+ * - Validaciones de formularios
+ * - Analizar tráfico de datos y planes de despliegue
+ * - Desplegar backend, frontend y base de datos
+ * - Configurar entornos de desarrollo y producción
+ * - Configurar CI/CD
+ * - Buscar diseños de UI UX
+ * - Optimizar llamados al server
+ * - Hacer UI de loading
+ * - Hacer UI de error
+ * - Agregar microinteracciones
+ * - Agregar animaciones
+ * - Hacer servicio de mailing para recuperar contraseña y verificar correo
+ * - Hacer lógica de familias (Hasta 5 personas)
+ */
