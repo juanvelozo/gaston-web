79335ce69437db401f4dbfdda86a6f996b3165f8
juanvelozo <juanvelozomoreno18@gmail.com>
2025-06-20 18:17:22 -0300

    agrega propiedad replace en private routes

--- src/navigator/PrivateRoute.tsx
+++ src/navigator/PrivateRoute.tsx
@@ -4,7 +4,7 @@ import { Navigate, Outlet } from "react-router-dom";
 const PrivateRoute = () => {
   const token = localStorage.getItem("access_token");
 
-  return token ? <Outlet /> : <Navigate to="/login" />;
+  return token ? <Outlet /> : <Navigate to="/login" replace />;
 };
 
 export default PrivateRoute;
