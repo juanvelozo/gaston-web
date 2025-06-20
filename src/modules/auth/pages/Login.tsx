0e5d9494c78dab4e4a058ed519a7a2fba8530952
juanvelozo <juanvelozomoreno18@gmail.com>
2025-06-20 19:10:58 -0300

    [AUTH] prototipo de formulario login

--- src/modules/auth/pages/Login.tsx
+++ src/modules/auth/pages/Login.tsx
@@ -1,7 +1,36 @@
-const LoginScreen = ({}: ILoginScreen): React.JSX.Element => {
+import { useState } from "react";
+import { useLogin } from "../hooks/useLogin.hook";
+import { ILoginRequest } from "../model/auth.model";
+
+const LoginScreen = (): React.JSX.Element => {
+  const { signIn } = useLogin();
+
+  const [formData, setFormData] = useState<ILoginRequest>({
+    email: "",
+    password: "",
+  });
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>): void {
+    e.preventDefault();
+    signIn(formData);
+  }
+
   return (
     <div>
       <span>LoginScreen</span>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Email:
+          <input type="email" name="email" onChange={(e) => setFormData({ ...formData, email: e.target.value })} />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input type="password" name="password" onChange={(e) => setFormData({ ...formData, password: e.target.value })} />
+        </label>
+        <br />
+        <button type="submit">Iniciar sesión</button>
+      </form>
     </div>
   );
 };
