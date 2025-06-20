728dab9d635da73e69f2a1981ea5db1a86c41996
juanvelozo <juanvelozomoreno18@gmail.com>
2025-06-20 19:10:44 -0300

    [AUTH] actualiza response de endpoint login

--- src/modules/auth/api/Login.api.ts
+++ src/modules/auth/api/Login.api.ts
@@ -2,14 +2,14 @@ import api from "../../../api/api";
 import { IAuthResponse } from "../model/auth.controller";
 import { ILoginRequest } from "../model/auth.model";
 
-export async function postLogin(body: ILoginRequest) {
+export async function postLogin(body: ILoginRequest): Promise<IAuthResponse> {
   try {
     console.log("Iniciando sesión...");
 
-    const response = await api.post<IAuthResponse>("/auth/login", body);
+    const response = await api.post<IAuthResponse>("/auth/signin", body);
 
     console.log("Sesión iniciada exitosamente", { response });
-    return response;
+    return response.data;
   } catch (error) {
     console.log("Hubo un error al iniciar sesión", error);
     throw error;
