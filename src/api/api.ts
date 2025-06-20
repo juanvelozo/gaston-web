f004dc7534c78f210dfe3d2e7192653bbcf2c7d4
juanvelozo <juanvelozomoreno18@gmail.com>
2025-06-20 17:26:27 -0300

    [API] agrega servicio de axios

---
+++ src/api/api.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://localhost:3000",
+});
+
+// Interceptor para agregar el token a cada request
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("access_token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+export default api;
