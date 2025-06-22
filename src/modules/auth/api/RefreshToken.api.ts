40bf686bd201783384762e15f07cf5983c932344
juanvelozo <juanvelozomoreno18@gmail.com>
2025-06-22 18:53:38 -0300

    [AUTH] Arregla request de endpoint refresh token

--- src/modules/auth/api/RefreshToken.api.ts
+++ src/modules/auth/api/RefreshToken.api.ts
@@ -1,13 +1,13 @@
 import api from "../../../api/api";
-import { IAuthResponse } from "../model/auth.controller";
+import { ITokens } from "../model/auth.model";
 
-export async function postRefreshToken(refreshToken: string): Promise<IAuthResponse> {
+export async function postRefreshToken(userId: number, refreshToken: string): Promise<ITokens> {
   try {
     console.log("Actualizando token...");
 
-    const response = await api.post<IAuthResponse>("/auth/refresh", { refreshToken });
+    const response = await api.post<ITokens>("/auth/refresh", { userId, refreshToken });
 
-    console.log("Token actualizado exitosamente", { response });
+    console.log("Token actualizado exitosamente");
     return response.data;
   } catch (error) {
     console.error("Error en refresh token", error);
