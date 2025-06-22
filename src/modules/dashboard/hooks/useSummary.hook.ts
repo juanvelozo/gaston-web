ba1e86ba2e64e4237eb85e9c98a789f6f619d1b2
juanvelozo <juanvelozomoreno18@gmail.com>
2025-06-22 18:55:20 -0300

    [DASHBOARD] agrega hook para usar endpoint de resumen

---
+++ src/modules/dashboard/hooks/useSummary.hook.ts
@@ -0,0 +1,8 @@
+import { useEndpoint } from "../../../hooks/useEndpoint";
+import { getSummary } from "../api/Summary.api";
+
+export const useSummary = () => {
+  const query = useEndpoint({ endpoint: getSummary, immediate: true });
+
+  return query;
+};
