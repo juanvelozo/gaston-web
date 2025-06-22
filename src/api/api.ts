486c33d71a07b97b5e15707b22a35cf2e6298ff4
juanvelozo <juanvelozomoreno18@gmail.com>
2025-06-22 18:54:06 -0300

    [API] agrega lógica de redireccionar al fallar refresh token

--- src/api/api.ts
+++ src/api/api.ts
@@ -23,26 +23,43 @@ api.interceptors.response.use(
   async (error: AxiosError) => {
     const originalRequest = error.config;
 
+    const falloRefrescarToken = error.response?.status === 403 && originalRequest?.url === "/auth/refresh";
+
+    if (falloRefrescarToken) {
+      console.log("Falló al refrescar token, redirigiendo...");
+      localStorage.clear();
+      window.location.href = "/login"; // redirección segura
+      return Promise.reject(error);
+    }
+
     // Solo intentar refresh si no lo intentamos antes
     if (error.response?.status === 401) {
       console.log("Token expirado, intentando actualizar el token...");
 
       const refreshToken = localStorage.getItem("refresh_token");
+      const userId = localStorage.getItem("user_id");
+
       if (!refreshToken) {
         throw new Error("No refresh token");
       }
 
-      const response = await postRefreshToken(refreshToken);
+      const response = await postRefreshToken(Number(userId), refreshToken);
 
       if (response.access_token) {
-        const newAccessToken = response.access_token;
+        localStorage.removeItem("access_token");
+        localStorage.removeItem("refresh_token");
 
+        const newAccessToken = response.access_token;
+        const newRefreshToken = response.refresh_token;
+
+        localStorage.setItem("refresh_token", newRefreshToken);
         localStorage.setItem("access_token", newAccessToken);
 
         // Volver a intentar la request original con el nuevo token
         if (originalRequest && originalRequest.headers) {
           originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
         }
+
         return api(originalRequest!);
       } else {
         localStorage.removeItem("access_token");
