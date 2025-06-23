d4a7432b6153acdb302f7ce1d9f447f5c943b9fa
juanvelozo <juanvelozomoreno18@gmail.com>
2025-06-23 09:53:38 -0300

    [DASHBOARD] agrega shortcuts a borrar y detalle de transaccion

--- src/modules/dashboard/pages/Dashboard.tsx
+++ src/modules/dashboard/pages/Dashboard.tsx
@@ -1,42 +1,59 @@
-import { Link } from "react-router-dom";
-import { useLogout } from "../../auth/hooks/useLogout.hook";
-import { useTransactions } from "../../transactions/hooks/useTransactions.hook";
-import { useSummary } from "../hooks/useSummary.hook";
+import { Link, useNavigate } from 'react-router-dom';
+import { useLogout } from '../../auth/hooks/useLogout.hook';
+import { useTransactions } from '../../transactions/hooks/useTransactions.hook';
+import { useSummary } from '../hooks/useSummary.hook';
+import { useEffect } from 'react';
+import { formatearMonto } from '../../../types/formatearMonto';
 
 const DashboardPages = (): React.JSX.Element => {
-  const { data: summary } = useSummary();
+  const navigate = useNavigate();
+  const { data: summary, refetch } = useSummary();
   const {
     allTransactions: { data },
+    borrar,
   } = useTransactions();
   const { cerrarSesion } = useLogout();
+
+  useEffect(() => {
+    async function reloadSummary() {
+      await refetch();
+    }
+
+    reloadSummary();
+  }, [data, refetch]);
 
   return (
     <div>
       <button onClick={() => cerrarSesion()}>Cerrar sesión</button>
       <div>
         <h2>Resumen</h2>
-        <div style={{ display: "flex", justifyContent: "space-around" }}>
+        <div style={{ display: 'flex', justifyContent: 'space-around' }}>
           <div>
             <h2>Gastos</h2>
-            <h4>{summary?.data.expense}</h4>
+            <h4>{formatearMonto(summary?.data.expense!)}</h4>
           </div>
           <div>
             <h2>Ingresos</h2>
-            <h4>{summary?.data.income}</h4>
+            <h4>{formatearMonto(summary?.data.income!)}</h4>
           </div>
           <div>
             <h2>Ahorros</h2>
-            <h4>{summary?.data.saving}</h4>
+            <h4>{formatearMonto(summary?.data.saving!)}</h4>
           </div>
         </div>
         <div>
-          <h2>Transacciones ({data?.data.length})</h2>
+          <h2>Últimas transacciones</h2>
           <Link to="/transactions/create">Crear transacción</Link>
-          <div style={{ marginTop: "1rem" }}>
-            {data?.data.map((transaction) => (
-              <div key={transaction.id}>
-                <span>{transaction.type === "EXPENSE" ? "- " : "+ "}</span>
-                {transaction.title} - {transaction.amount}
+          <div style={{ marginTop: '1rem' }}>
+            {data?.data.slice(0, 3).map((transaction) => (
+              <div key={transaction.id} style={{ display: 'flex', justifyContent: 'space-evenly' }}>
+                <span>{transaction.type === 'EXPENSE' ? '- ' : '+ '}</span>
+                <p>{transaction.title}</p>
+                <p> {formatearMonto(transaction.amount)}</p>
+                <button onClick={() => navigate(`/transactions/${transaction.id}`)}>
+                  Detalle de la transacción
+                </button>
+                <button onClick={() => borrar(transaction.id)}>Eliminar</button>
               </div>
             ))}
           </div>
