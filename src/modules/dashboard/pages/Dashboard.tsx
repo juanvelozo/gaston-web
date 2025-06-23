b7f153fa2b7f7bd9eff480a903dd8f9574c3b909
juanvelozo <juanvelozomoreno18@gmail.com>
2025-06-22 22:27:57 -0300

    [DASHBOARD] cambia html del home

--- src/modules/dashboard/pages/Dashboard.tsx
+++ src/modules/dashboard/pages/Dashboard.tsx
@@ -7,7 +7,12 @@ const DashboardPages = (): React.JSX.Element => {
 
   return (
     <div>
-      <span>{JSON.stringify(summary)}</span>
+      <h2>Gastos</h2>
+      <h4>{summary?.data.expense}</h4>
+      <h2>Ingresos</h2>
+      <h4>{summary?.data.income}</h4>
+      <h2>Ahorros</h2>
+      <h4>{summary?.data.saving}</h4>
       <button onClick={() => cerrarSesion()}>Cerrar sesión</button>
     </div>
   );
