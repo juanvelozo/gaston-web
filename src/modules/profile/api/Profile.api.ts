1e06ba056dd4a1d4098673994021019613480b2b
juanvelozo <juanvelozomoreno18@gmail.com>
2025-06-23 19:55:37 -0300

    [PROFILE] integra api de profile

---
+++ src/modules/profile/api/Profile.api.ts
@@ -0,0 +1,52 @@
+import { AxiosError } from 'axios';
+import api from '../../../api/api';
+import {
+  IGetProfileResponse,
+  IUpdatePasswordDto,
+  IUpdateProfileDto,
+} from '../model/profile.controller';
+
+export async function getProfile(): Promise<IGetProfileResponse | undefined> {
+  try {
+    console.log('Obteniendo perfil...');
+    const response = await api.get<IGetProfileResponse>('/user/profile');
+
+    console.log('Perfil obtenido exitosamente');
+    return response.data;
+  } catch (error) {
+    console.error('Hubo un error al obtener el perfil', error);
+    throw error;
+  }
+}
+
+export async function updateProfile(body: IUpdateProfileDto) {
+  try {
+    console.log('Actualizando perfil...', body);
+    const response = await api.patch('/user/profile', body);
+
+    console.log('Perfil actualizado exitosamente');
+    return response.data;
+  } catch (error) {
+    console.error('Hubo un error al actualizar el perfil', error);
+    throw error;
+  }
+}
+
+export async function changePassword(body: IUpdatePasswordDto) {
+  try {
+    console.log('Actualizando perfil...', body);
+    const response = await api.patch('/user/change-password', body);
+
+    console.log('Perfil actualizado exitosamente');
+    return response.data;
+  } catch (error) {
+    console.error('Hubo un error al actualizar el perfil', error);
+    throw error;
+    // if ((error as AxiosError).response) {
+    //   const axiosError = error as AxiosError;
+
+    //   const { status, data } = axiosError.response!;
+    //   throw axiosError;
+    // }
+  }
+}
