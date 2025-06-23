d8e029bd758995ef2bb7bb58fb6ad5ee067c1730
juanvelozo <juanvelozomoreno18@gmail.com>
2025-06-23 15:28:48 -0300

    [CATEGORY] agrega modelos

---
+++ src/modules/category/model/category.model.ts
@@ -0,0 +1,10 @@
+export interface ICategory {
+  id: number;
+  name: string;
+  description: string;
+  color: string;
+  icon: string;
+  createdAt: string;
+  updatedAt: string;
+  userId: number;
+}
