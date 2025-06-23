1b6aa5499bc32ed3903cd08611a96f2b3ac6028e
juanvelozo <juanvelozomoreno18@gmail.com>
2025-06-23 09:51:50 -0300

    [TRANSACTIONS] code format y actualiza responses

--- src/modules/transactions/api/transactions.api.ts
+++ src/modules/transactions/api/transactions.api.ts
@@ -1,67 +1,73 @@
-import api from "../../../api/api";
-import { ICreateTransactionDto, ICreateTransactionResponse, IGetAllTransactionsResponse, IUpdateTransactionDto } from "../model/transaction.controller";
+import api from '../../../api/api';
+import {
+  ICreateTransactionDto,
+  ICreateTransactionResponse,
+  IGetAllTransactionsResponse,
+  IGetTransactionResponse,
+  IUpdateTransactionDto,
+} from '../model/transaction.controller';
 
 export async function fetchAllTransactions() {
   try {
-    console.log("Obteniendo la lista de transacciones...");
-    const response = await api.get<IGetAllTransactionsResponse>("/transactions");
+    console.log('Obteniendo la lista de transacciones...');
+    const response = await api.get<IGetAllTransactionsResponse>('/transactions');
 
-    console.log("Lista de transacciones obtenida.");
+    console.log('Lista de transacciones obtenida.');
     return response.data;
   } catch (error) {
-    console.error("Hubo un error al obtener la lista de transacciones", error);
+    console.error('Hubo un error al obtener la lista de transacciones', error);
     Promise.reject(error);
   }
 }
 
 export async function createTransaction(body: ICreateTransactionDto) {
   try {
-    console.log("Creando transaccion...");
-    const response = await api.post<ICreateTransactionResponse>("/transactions", body);
+    console.log('Creando transaccion...');
+    const response = await api.post<ICreateTransactionResponse>('/transactions', body);
 
-    console.log("Transaccion creada exitosamente");
+    console.log('Transaccion creada exitosamente');
     return response.data;
   } catch (error) {
-    console.error("Hubo un error al crear la transaccion", error);
+    console.error('Hubo un error al crear la transaccion', error);
     Promise.reject(error);
   }
 }
 
 export async function deleteTransaction(id: number) {
   try {
-    console.log("Eliminando transaccion...");
+    console.log('Eliminando transaccion...');
     const response = await api.delete(`/transactions/${id}`);
 
-    console.log("Transaccion eliminada exitosamente");
+    console.log('Transaccion eliminada exitosamente');
     return response.data;
   } catch (error) {
-    console.error("Hubo un error al eliminar la transaccion", error);
+    console.error('Hubo un error al eliminar la transaccion', error);
     Promise.reject(error);
   }
 }
 
 export async function updateTransaction(id: number, body: IUpdateTransactionDto) {
   try {
-    console.log("Actualizando transaccion...", body);
+    console.log('Actualizando transaccion...', body);
     const response = await api.patch(`/transactions/${id}`, body);
 
-    console.log("Transaccion actualizada exitosamente");
+    console.log('Transaccion actualizada exitosamente');
     return response.data;
   } catch (error) {
-    console.error("Hubo un error al actualizar la transaccion", error);
+    console.error('Hubo un error al actualizar la transaccion', error);
     Promise.reject(error);
   }
 }
 
-export async function getTransactionById(id: number) {
+export async function getTransactionById(id: number): Promise<IGetTransactionResponse | undefined> {
   try {
-    console.log("Obteniendo transaccion...");
-    const response = await api.get(`/transactions/${id}`);
+    console.log('Obteniendo transaccion...');
+    const response = await api.get<IGetTransactionResponse>(`/transactions/${id}`);
 
-    console.log("Transaccion obtenida exitosamente");
+    console.log('Transaccion obtenida exitosamente');
     return response.data;
   } catch (error) {
-    console.error("Hubo un error al obtener la transaccion", error);
+    console.error('Hubo un error al obtener la transaccion', error);
     Promise.reject(error);
   }
 }
