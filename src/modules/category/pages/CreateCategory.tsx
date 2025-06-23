eb7d9ffc243d5a21542afcfa3861b282c7c7c6c6
juanvelozo <juanvelozomoreno18@gmail.com>
2025-06-23 17:28:09 -0300

    [CATEGORY] consume api de crear categoría

--- src/modules/category/pages/CreateCategory.tsx
+++ src/modules/category/pages/CreateCategory.tsx
@@ -1,12 +1,55 @@
+import { useState } from 'react';
+import { ICreateCategoryDto } from '../model/category.controller';
+import { useCategories } from '../hooks/useCategories.hook';
+import EmojiButtonPicker from '../../../components/common/emojiPicker/EmojiPicker.component';
+
 const CreateCategoryPage = (): React.JSX.Element => {
+  const [formData, setFormData] = useState<ICreateCategoryDto>({
+    name: '',
+    description: '',
+    color: '',
+    icon: '😉',
+  });
+
+  const { crear } = useCategories();
+
   return (
     <div>
       <h2>Crear categoría</h2>
       <br />
-      <form>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          crear(formData);
+        }}
+      >
         <label>Nombre:</label>
-        <input type="text" name="name" required />
+        <input
+          type="text"
+          name="name"
+          required
+          onChange={(e) => setFormData({ ...formData, name: e.target.value })}
+        />
         <br />
+        <label>Descripción:</label>
+        <input
+          type="text"
+          name="description"
+          onChange={(e) => setFormData({ ...formData, description: e.target.value })}
+        />
+        <br />
+        <label>Color:</label>
+        <input
+          type="color"
+          name="color"
+          required
+          onChange={(e) => setFormData({ ...formData, color: e.target.value })}
+        />
+        <br />
+        <label>Icono:</label>
+        <EmojiButtonPicker
+          onChange={(emoji: string) => setFormData({ ...formData, icon: emoji })}
+        />
         <button type="submit">Crear</button>
       </form>
     </div>
