a0b0f3690223e61bae963ce8734b4ec6f5dad2f6
juanvelozo <juanvelozomoreno18@gmail.com>
2025-06-23 19:54:59 -0300

    [PROFILE] agrega controladores

---
+++ src/modules/profile/model/profile.controller.ts
@@ -0,0 +1,23 @@
+import { IBaseResponse } from '../../../types/proyect.model';
+import { IUser } from '../../auth/model/auth.model';
+
+/**
+ * Editar perfil
+ */
+export type IUpdateProfileDto = {
+  fullName?: string;
+  profile_photo?: string;
+};
+
+/**
+ * Cambiar contraseña
+ */
+export type IUpdatePasswordDto = {
+  currentPassword: string;
+  newPassword: string;
+};
+
+/**
+ * Perfil
+ */
+export type IGetProfileResponse = IBaseResponse<IUser>;
