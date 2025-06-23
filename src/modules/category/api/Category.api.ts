d19fb39d8db5c47559871005c079ec9a13a4ed5f
juanvelozo <juanvelozomoreno18@gmail.com>
2025-06-23 16:13:10 -0300

    [CATEGORY] integra crud de categorías

---
+++ src/modules/category/api/Category.api.ts
@@ -0,0 +1,76 @@
+import api from '../../../api/api';
+import {
+  ICreateCategoryDto,
+  ICreateCategoryResponse,
+  IDeleteCategoryResponse,
+  IGetAllCategoriesResponse,
+  IGetCategoryResponse,
+  IUpdateCategoryDto,
+} from '../model/category.controller';
+
+export async function fetchAllCategories(): Promise<IGetAllCategoriesResponse | undefined> {
+  try {
+    console.log('Obteniendo la lista de categorias...');
+    const response = await api.get<IGetAllCategoriesResponse>('/categories');
+
+    console.log('Lista de categorias obtenida.');
+    return response.data;
+  } catch (error) {
+    console.error('Hubo un error al obtener la lista de categorias', error);
+    Promise.reject(error);
+  }
+}
+
+export async function createCategory(
+  body: ICreateCategoryDto
+): Promise<ICreateCategoryResponse | undefined> {
+  try {
+    console.log('Creando categoria...');
+    const response = await api.post<ICreateCategoryResponse>('/categories', body);
+
+    console.log('Categoria creada exitosamente');
+    return response.data;
+  } catch (error) {
+    console.error('Hubo un error al crear la categoria', error);
+    Promise.reject(error);
+  }
+}
+
+export async function deleteCategory(id: number): Promise<IDeleteCategoryResponse | undefined> {
+  try {
+    console.log('Eliminando categoria...');
+    const response = await api.delete<IDeleteCategoryResponse>(`/categories/${id}`);
+
+    console.log('Categoria eliminada exitosamente');
+    return response.data;
+  } catch (error) {
+    console.error('Hubo un error al eliminar la categoria', error);
+    Promise.reject(error);
+  }
+}
+
+export async function updateCategory(id: number, body: IUpdateCategoryDto) {
+  try {
+    console.log('Actualizando categoria...', body);
+    const response = await api.patch(`/categories/${id}`, body);
+
+    console.log('Categoria actualizada exitosamente');
+    return response.data;
+  } catch (error) {
+    console.error('Hubo un error al actualizar la categoria', error);
+    Promise.reject(error);
+  }
+}
+
+export async function getCategoryById(id: number): Promise<IGetCategoryResponse | undefined> {
+  try {
+    console.log('Obteniendo categoria...');
+    const response = await api.get<IGetCategoryResponse>(`/categories/${id}`);
+
+    console.log('Categoria obtenida exitosamente');
+    return response.data;
+  } catch (error) {
+    console.error('Hubo un error al obtener la categoria', error);
+    Promise.reject(error);
+  }
+}
