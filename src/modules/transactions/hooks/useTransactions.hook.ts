badbb555f5e4088cac8b7e452049b9fca1410085
juanvelozo <juanvelozomoreno18@gmail.com>
2025-06-23 15:28:37 -0300

    [TRANSACTIONS] code format

--- src/modules/transactions/hooks/useTransactions.hook.ts
+++ src/modules/transactions/hooks/useTransactions.hook.ts
@@ -1,27 +1,36 @@
-import { useLocation, useNavigate } from "react-router-dom";
-import { useEndpoint } from "../../../hooks/useEndpoint";
-import { createTransaction, deleteTransaction, fetchAllTransactions, getTransactionById, updateTransaction } from "../api/transactions.api";
-import { ICreateTransactionDto, IUpdateTransactionDto } from "../model/transaction.controller";
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useEndpoint } from '../../../hooks/useEndpoint';
+import {
+  createTransaction,
+  deleteTransaction,
+  fetchAllTransactions,
+  getTransactionById,
+  updateTransaction,
+} from '../api/transactions.api';
+import { ICreateTransactionDto, IUpdateTransactionDto } from '../model/transaction.controller';
 
 export const useTransactions = () => {
   const navigate = useNavigate();
   const { pathname } = useLocation();
-  const allTransactions = useEndpoint({ endpoint: fetchAllTransactions, immediate: true });
+  const allTransactions = useEndpoint({
+    endpoint: fetchAllTransactions,
+    immediate: pathname === '/transactions' || pathname === '/',
+  });
   const create = useEndpoint({ endpoint: createTransaction });
   const update = useEndpoint({ endpoint: updateTransaction });
   const search = useEndpoint({ endpoint: getTransactionById });
   const eliminate = useEndpoint({ endpoint: deleteTransaction });
 
   async function crear(transaction: ICreateTransactionDto) {
-    await create.call(transaction).then(() => navigate("/", { replace: true }));
+    await create.call(transaction).then(() => navigate('/', { replace: true }));
   }
   async function editar(id: number, transaction: IUpdateTransactionDto) {
-    await update.call(id, transaction).then(() => navigate("/", { replace: true }));
+    await update.call(id, transaction).then(() => navigate('/', { replace: true }));
   }
   async function borrar(id: number) {
     await eliminate.call(id).then(async () => {
-      if (pathname !== "/") {
-        navigate("/", { replace: true });
+      if (pathname !== '/') {
+        navigate('/', { replace: true });
       }
       await allTransactions.refetch();
     });
