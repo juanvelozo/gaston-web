1e93aec6e739943c436fe6a440faa2746b97d431
juanvelozo <juanvelozomoreno18@gmail.com>
2025-06-23 09:52:23 -0300

    agrega función para formatear dinero

---
+++ src/types/formatearMonto.tsx
@@ -0,0 +1,7 @@
+export const formatearMonto = (monto: number): string => {
+  return new Intl.NumberFormat('es-AR', {
+    style: 'currency',
+    currency: 'ARS',
+    minimumFractionDigits: 2,
+  }).format(monto);
+};
