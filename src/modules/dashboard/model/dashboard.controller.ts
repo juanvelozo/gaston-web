e403f06c3345b4f34a0166ff1fdea7bf601adca6
juanvelozo <juanvelozomoreno18@gmail.com>
2025-06-22 22:27:27 -0300

    [DASHBOARD] agrega modelos y controladores

---
+++ src/modules/dashboard/model/dashboard.controller.ts
@@ -0,0 +1,4 @@
+import { IBaseResponse } from "../../../types/proyect.model";
+import { ISummary } from "./dashboard.model";
+
+export type IGetSummaryResponse = IBaseResponse<ISummary>;
---
+++ src/modules/dashboard/model/dashboard.model.ts
@@ -0,0 +1,5 @@
+export interface ISummary {
+  income: number;
+  expense: number;
+  saving: number;
+}
