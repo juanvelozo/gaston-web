865aea5942ca27b8f7bfd964c1c92f595fdf774b
juanvelozo <juanvelozomoreno18@gmail.com>
2025-07-02 12:19:34 -0300

    [TRANSACTIONS] implementa itemLists en transactionCard

--- src/modules/transactions/components/transactionCard/transactionCard.component.tsx
+++ src/modules/transactions/components/transactionCard/transactionCard.component.tsx
@@ -3,27 +3,18 @@ import { formatearMonto } from '../../../../types/formatearMonto';
 import { ITransaction, TransactionType } from '../../model/transactions.model';
 import { ReactNode } from 'react';
 import { ArrowDownRightCircle, ArrowUpRightCircle, PiggyBank } from 'iconoir-react';
-import { motion } from 'framer-motion';
+import ItemList from '../../../../components/common/ItemList/ItemList.component';
 const TransactionCard = ({ data }: ITransactionCard): React.JSX.Element => {
   const navigate = useNavigate();
 
   return (
-    <motion.div
-      key={data.id}
-      className="flex items-center gap-4 p-4 rounded-2xl bg-white border border-gray-100  w-full justify-between cursor-pointer"
+    <ItemList
       onClick={() => navigate(`/transactions/${data.id}`)}
-      whileTap={{ scale: 0.97 }}
-      whileHover={{ scale: 1.05 }}
-    >
-      <div className="flex items-center justify-center gap-3">
-        <span>{ITransactionCardValues[data.type].icon}</span>
-        <div>
-          <p className=" text-lg">{data.title}</p>
-          <p className="text-gray-600 text-sm">{data.category.name}</p>
-        </div>
-      </div>
-      <p className="text-xl"> {formatearMonto(data.amount)}</p>
-    </motion.div>
+      icon={ITransactionCardValues[data.type].icon}
+      title={data.title}
+      label={data.category.name}
+      value={formatearMonto(data.amount)}
+    />
   );
 };
 interface ITransactionCard {
@@ -37,7 +28,7 @@ export type TypeConfig = {
   };
 };
 
-const ITransactionCardValues: TypeConfig = {
+export const ITransactionCardValues: TypeConfig = {
   EXPENSE: { title: 'Gasto', icon: <ArrowDownRightCircle color="#FE5F55" /> },
   INCOME: { title: 'Ingreso', icon: <ArrowUpRightCircle color="#3A7D44" /> },
   SAVING: { title: 'Ahorro', icon: <PiggyBank color="#F2AF29" /> },
