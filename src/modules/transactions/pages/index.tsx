a5e6738b298f7bd6b1de33ef0260764e1adf608e
juanvelozo <juanvelozomoreno18@gmail.com>
2025-07-02 18:05:53 -0300

    [TRANSACTION] restaura acceso a formulario de crear

--- src/modules/transactions/pages/index.tsx
+++ src/modules/transactions/pages/index.tsx
@@ -4,18 +4,20 @@ import SectionHeader from '../../../components/common/sectionHeader/sectionHeade
 import IconButton from '../../../components/common/iconButton/iconButton.component';
 import { Plus } from 'iconoir-react';
 import colors from '../../../styles/colors';
+import { useNavigate } from 'react-router-dom';
 
 export const TransactionsPage = (): React.JSX.Element => {
   const {
     allTransactions: { data },
   } = useTransactions();
+  const navigate = useNavigate();
 
   return (
     <div className="bg-white dark:bg-zinc-900 dark:border-zinc-700 shadow-sm rounded-2xl space-y-2">
       <SectionHeader
         title="Transacciones"
         bgColor={colors.green}
-        right={<IconButton icon={<Plus />} />}
+        right={<IconButton icon={<Plus />} onClick={() => navigate('/transactions/create')} />}
       />
       <div className="flex flex-col gap-4 p-4">
         {data?.data.map((transaction) => (
