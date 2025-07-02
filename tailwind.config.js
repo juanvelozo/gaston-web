8f8e8849e091fe871d4b3e58a0772fb6227addd0
juanvelozo <juanvelozomoreno18@gmail.com>
2025-07-02 10:01:41 -0300

    agrega colores del theme v1

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          coral: '#FE5F55',
+          white: '#FFFFFF',
+          green: '#3A7D44',
+          yellow: '#F2AF29',
+          black: '#000000',
+        },
+      },
+    },
   },
   plugins: [],
 };
