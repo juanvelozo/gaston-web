a8a101f45c6991a09b3425fcfb8663b347a918af
juanvelozo <juanvelozomoreno18@gmail.com>
2025-07-02 12:19:57 -0300

    [COMPONENTS] refactoriza sectionHeader

--- src/components/common/sectionHeader/sectionHeader.component.tsx
+++ src/components/common/sectionHeader/sectionHeader.component.tsx
@@ -1,10 +1,10 @@
-import { DollarCircle, Home, List, Plus, UserCircle } from 'iconoir-react';
-import { Button } from '../../animated/button/Button.component';
-import { PrivateRouteConfig, PrivateRoutesType } from '../../navigation/Navbar/Navbar.component';
-import { useNavigate } from 'react-router-dom';
-
-const SectionHeader = ({ bgColor, left, right, title }: ISectionHeader): React.JSX.Element => {
-  const navigate = useNavigate();
+const SectionHeader = ({
+  bgColor,
+  left,
+  right,
+  title,
+  bottom,
+}: ISectionHeader): React.JSX.Element => {
   return (
     <div>
       <div className={`bg-[${bgColor}] p-6 rounded-b-3xl`}>
@@ -15,6 +15,7 @@ const SectionHeader = ({ bgColor, left, right, title }: ISectionHeader): React.J
           </div>
           {right}
         </div>
+        <div className="flex items-center justify-center p-2">{bottom}</div>
       </div>
     </div>
   );
@@ -24,6 +25,7 @@ interface ISectionHeader {
   title?: string;
   left?: React.ReactNode;
   right?: React.ReactNode;
+  bottom?: React.ReactNode;
 }
 
 export default SectionHeader;
