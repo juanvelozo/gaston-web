8a7f872fcf07d62f2626cb066fc7544b3dc6d4d9
juanvelozo <juanvelozomoreno18@gmail.com>
2025-07-04 18:11:12 -0300

    [COMPONENTS] sectionHeader > arregla bug al tener título muy largo

--- src/components/common/sectionHeader/sectionHeader.component.tsx
+++ src/components/common/sectionHeader/sectionHeader.component.tsx
@@ -9,11 +9,13 @@ const SectionHeader = ({
     <div>
       <div className={`p-6 rounded-b-3xl`} style={{ background: bgColor }}>
         <div className="flex gap-2 justify-between items-center my-4">
-          {left}
-          <div className="flex gap-2 items-center max-w-[70%] overflow-hidden">
-            <h1 className="text-3xl font-bold text-white truncate text-ellipsis ">{title}</h1>
+          <div>{left}</div>
+          <div className="flex gap-2 items-center max-w-[70%] w-full overflow-hidden place-self-center">
+            <h1 className="text-3xl font-bold text-white text-center truncate text-ellipsis ">
+              {title}
+            </h1>
           </div>
-          {right}
+          <div>{right}</div>
         </div>
         <div className="flex items-center justify-center p-2">{bottom}</div>
       </div>
