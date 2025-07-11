a2b10f10365832d35cfa9cddf983b5038cdd1634
juanvelozo <juanvelozomoreno18@gmail.com>
2025-07-11 15:12:16 -0300

    [STYLES] modifica colores del theme

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -6,10 +6,9 @@ const colors = {
   yellow: '#F2AF29',
   black: '#101720',
   teal: '#2A9D8F', // Complementa el coral y el amarillo
-  beige: '#F4EBD9', // Neutro cálido que suaviza el conjunto
   navy: '#1E2A38', // Un azul más profundo que hace buen contraste con coral y amarillo
   olive: '#7D883B', // Tierra/verde más apagado, combina con verde y mostaza
-  softPink: '#F6B6B6', // Acompaña bien al coral sin competir
+  softPink: '#F1A9F0', // Acompaña bien al coral sin competir
   burntOrange: '#D95D39', // Más terroso que el coral, muy armónico
   skyBlue: '#69A9D0', // Contraste con el azul y equilibrio con el coral
 };
