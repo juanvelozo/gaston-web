bf79e216b1087cb94350c07653b2c7f7717391d0
juanvelozo <juanvelozomoreno18@gmail.com>
2025-07-12 21:00:55 -0300

    actualiza el handleApiError para abarcar más errores

--- src/api/apiError.ts
+++ src/api/apiError.ts
@@ -3,13 +3,47 @@ import { toast } from 'sonner';
 import { ICustomError } from '../types/proyect.model';
 
 export function handleApiError(error: unknown): never {
-  if (error instanceof AxiosError) {
-    const data = error.response?.data as ICustomError;
+  console.log('Ocurrió un error en la petición', error);
 
-    if (data?.message && Array.isArray(data.message)) {
-      data.message.forEach((m) => toast.error(m));
+  if (error instanceof AxiosError) {
+    const isTimeout = error.code === 'ECONNABORTED';
+    const noResponse = !error.response;
+
+    if (isTimeout) {
+      toast.error('Ocurrió un error', {
+        description: ERROR_MESSAGES.TIMEOUT,
+      });
+    } else if (noResponse) {
+      toast.error('Ocurrió un error', {
+        description: ERROR_MESSAGES.NO_RESPONSE,
+      });
+    } else {
+      const data = error.response?.data as ICustomError;
+
+      if (data?.message && Array.isArray(data.message)) {
+        data.message.forEach((m) =>
+          toast.error('Ocurrió un error', {
+            description: m,
+          })
+        );
+      } else {
+        toast.error('Ocurrió un error', {
+          description: ERROR_MESSAGES.INVALID_FORMAT,
+        });
+      }
     }
+  } else {
+    toast.error('Ocurrió un error', {
+      description: ERROR_MESSAGES.UNKNOWN,
+    });
   }
 
-  throw error; // importante relanzarlo para que no se silencie
+  throw error;
 }
+
+const ERROR_MESSAGES = {
+  TIMEOUT: 'El servidor tardó demasiado en responder. Intentalo de nuevo más tarde.',
+  NO_RESPONSE: 'No se pudo conectar con el servidor. Verificá tu conexión a Internet.',
+  INVALID_FORMAT: 'Error inesperado en la respuesta del servidor.',
+  UNKNOWN: 'Se produjo un error desconocido.',
+};
