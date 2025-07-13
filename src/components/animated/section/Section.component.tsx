7e409787ac764dd00f2b25ad0161b7425a75f9d0
juanvelozo <juanvelozomoreno18@gmail.com>
2025-07-13 15:35:09 -0300

    [COMPONENTS] arregla estilos en section

--- src/components/animated/section/Section.component.tsx
+++ src/components/animated/section/Section.component.tsx
@@ -22,22 +22,23 @@ const Section = ({
   return (
     <div className="min-h-screen w-screen flex flex-col" style={{ background: bgColor }}>
       {/* HEADER sticky */}
-      <div className="sticky top-0 z-50 px-6 pt-6 pb-4" style={{ background: bgColor }}>
+      <div className="sticky top-0 z-20 px-6 pt-6 pb-4" style={{ background: bgColor }}>
         <div className="flex justify-between items-center mb-4">
-          <div>{left}</div>
+          <div className="min-w-10">{left}</div>
           <div className="flex-1 px-4 text-center">
-            <h1 className="text-2xl font-bold text-white truncate">{title}</h1>
+            <h1 className="text-2xl font-bold text-white truncate text-center">{title}</h1>
           </div>
-          <div>{right}</div>
+          <div className="min-w-10">{right}</div>
         </div>
-        {bottom && <div className="flex justify-center items-center">{bottom}</div>}
+        {bottom && <div className="flex justify-center items-center my-6">{bottom}</div>}
       </div>
 
       {/* BODY */}
       <div
         className={clsx(
-          'w-full bg-white rounded-t-3xl px-6 pt-6 pb-32',
-          tall ? 'flex-grow' : 'h-[75%]'
+          'w-full z-30 bg-white rounded-t-3xl px-6 pt-6 pb-32',
+          tall ? 'flex-grow' : 'h-[75%]',
+          'min-h-screen '
         )}
       >
         {children}
