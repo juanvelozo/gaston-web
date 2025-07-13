a87b807372d09e747db04bfb8191b7fd118b5b55
juanvelozo <juanvelozomoreno18@gmail.com>
2025-07-12 22:05:19 -0300

    [STYLES] actualiza theme

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,16 +1,16 @@
 const colors = {
   coral: '#FE5F55',
   white: '#FEFAFA',
-  blue: '#2A324B',
+  blue: '#1282a2',
   green: '#3A7D44',
   yellow: '#F2AF29',
   black: '#101720',
-  teal: '#2A9D8F', // Complementa el coral y el amarillo
-  navy: '#1E2A38', // Un azul más profundo que hace buen contraste con coral y amarillo
-  olive: '#7D883B', // Tierra/verde más apagado, combina con verde y mostaza
-  softPink: '#F1A9F0', // Acompaña bien al coral sin competir
-  burntOrange: '#D95D39', // Más terroso que el coral, muy armónico
-  skyBlue: '#69A9D0', // Contraste con el azul y equilibrio con el coral
+  teal: '#2A9D8F',
+  navy: '#1E2A38',
+  olive: '#7D883B',
+  softPink: '#F1A9F0',
+  burntOrange: '#D95D39',
+  skyBlue: '#69A9D0',
 };
 
 export const themeColors = Object.fromEntries(
