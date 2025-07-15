308c6d80c268411db636f5d7ce1a35bd2911a8d1
juanvelozo <juanvelozomoreno18@gmail.com>
2025-07-14 21:18:13 -0300

    [COMPONENTS] arregla estilos en section

--- src/components/animated/section/Section.component.tsx
+++ src/components/animated/section/Section.component.tsx
@@ -20,9 +20,15 @@ const Section = ({
   tall = false,
 }: SectionProps): React.JSX.Element => {
   return (
-    <div className="min-h-screen w-screen flex flex-col" style={{ background: bgColor }}>
+    <div
+      className="min-h-screen w-screen flex flex-col transition-colors duration-500 ease-in-out"
+      style={{ background: bgColor }}
+    >
       {/* HEADER sticky */}
-      <div className="sticky top-0 z-20 px-6 pt-6 pb-4" style={{ background: bgColor }}>
+      <div
+        className="sticky top-0 z-20 px-6 pt-6 pb-4 transition-colors duration-500 ease-in-out "
+        style={{ background: bgColor }}
+      >
         <div className="flex justify-between items-center mb-4">
           <div className="min-w-10">{left}</div>
           <div className="flex-1 px-4 text-center">
