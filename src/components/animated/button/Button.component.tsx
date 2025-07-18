3252b643c2eb2e8ce8bc0819c525596fd7a850b7
juanvelozo <juanvelozomoreno18@gmail.com>
2025-07-18 12:40:04 -0300

    [COMPONENTS] agrega spinner a button

--- src/components/animated/button/Button.component.tsx
+++ src/components/animated/button/Button.component.tsx
@@ -2,6 +2,7 @@ import { HTMLMotionProps, motion } from 'framer-motion';
 import React from 'react';
 import { cn } from '../../../libs/utils';
 import { IButtonProps, ButtonVariant } from './Button.types';
+import { FadeLoader } from 'react-spinners';
 
 const baseStyles =
   'inline-flex items-center justify-center px-4 py-2 rounded-2xl text-sm font-medium transition-colors focus:outline-none  disabled:opacity-50 disabled:pointer-events-none';
@@ -32,9 +33,7 @@ export const Button = React.forwardRef<HTMLButtonElement, IButtonProps>(
         {...props}
       >
         {loading ? (
-          <span
-            className={cn('h-4 w-4 rounded-full animate-spin border-2', loadingStyles[variant])}
-          />
+          <FadeLoader color="white" className="w-4 h-4" />
         ) : (
           <div className="flex items-center gap-2 min-w-0">
             {iconLeft && <span className="shrink-0">{iconLeft}</span>}
