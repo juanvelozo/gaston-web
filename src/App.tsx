73ed37677c2a9eeb835277b4b75ec8de8c6880e1
juanvelozo <juanvelozomoreno18@gmail.com>
2025-07-18 21:05:47 -0300

    actualiza comentarios en app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -23,21 +23,26 @@ export default App;
  * - Agregar assets para foto de perfil de usuario (Tal vez hacerla con IA?)✅
  * - Ver cómo agregar assets a la base de datos✅
  * - Integrar endpoints de cambiar contraseña✅
- * - Trabajar en el responsive mobile
- * - Arreglar tostadita en iOS
- * - Buscar librería de componentes/estilos
+ * - Trabajar en el responsive mobile✅
+ * - Arreglar tostadita en iOS✅
+ * - Buscar librería de componentes/estilos✅
  * - Las sesiones entre ambientes se comparten por alguna razón, solucionar
- * - Implementar estrategia de cacheo de datos
+ * - Implementar estrategia de cacheo de datos (con react query)
  * - Hacer endpoint de recupero de contraseña
+ * - Hacer ambiente de cloudinary para producción
+ * - Arreglar problemas de scroll
+ * - Implementar cookies en lugar de responder el token
  * - Validaciones de formularios
- * - Analizar tráfico de datos y planes de despliegue
+ * - En transacciones, hay que filtrar por mes
+ * - Implementar gráficos en la home y en transacciones (tal vez módulo aparte)
+ * - Analizar tráfico de datos y planes de despliegue✅
  * - Desplegar backend, frontend y base de datos✅
  * - Configurar entornos de desarrollo y producción✅
- * - Configurar CI/CD
+ * - Configurar CI/CD✅
  * - Buscar diseños de UI UX✅
  * - Optimizar llamados al server
- * - Hacer UI de loading
- * - Hacer UI de error
+ * - Hacer UI de loading✅
+ * - Hacer UI de error✅
  * - Agregar microinteracciones
  * - Agregar animaciones
  * - Hacer servicio de mailing para recuperar contraseña y verificar correo
