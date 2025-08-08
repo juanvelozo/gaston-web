24d487acb93a571de0ae9d9a847e02ecbe04e394
Juan Velozo <A05786@bh.com.ar>
2025-08-08 12:51:23 -0300

    [AUTH] agrega componente de sesion NFID button

---
+++ src/modules/auth/components/session/NFIDSessionButton.component.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Button } from '../../../../components/animated/button/Button.component';
+import { Internet } from 'iconoir-react';
+import { useAuth } from '../../../../hooks/useAuth.hook';
+
+export const NFIDButton: React.FC = () => {
+  const { iniciarSesion: handleLogin, cargando } = useAuth();
+
+  return (
+    <Button
+      variant="secondary"
+      onClick={handleLogin}
+      iconLeft={<Internet />}
+      disabled={cargando}
+      loading={cargando}
+    >
+      Iniciar sesión con NFID
+    </Button>
+  );
+};
