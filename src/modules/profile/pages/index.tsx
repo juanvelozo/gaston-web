30d34e572586b0a6dc5ac28db288e0f47d88ba46
Juan Velozo <A05786@bh.com.ar>
2025-08-08 12:51:55 -0300

    [PROFILE] agrega nuevo logout de NFID

--- src/modules/profile/pages/index.tsx
+++ src/modules/profile/pages/index.tsx
@@ -6,32 +6,32 @@ import { Button } from '../../../components/animated/button/Button.component';
 import { LogOut } from 'iconoir-react';
 import Section from '../../../components/animated/section/Section.component';
 import { Avatar } from '../../../components/common/avatar/Avatar.component';
+import { useAuth } from '../../../hooks/useAuth.hook';
 
 const ProfilePage = (): React.JSX.Element => {
-  const {
-    profile: { data, loading },
-  } = useProfile();
-
-  const { cerrarSesion } = useLogout();
+  // const {
+  //   profile: { data, loading },
+  // } = useProfile();
+  const { cerrarSesion: handleLogout } = useAuth();
 
   return (
     <div className="flex-1 min-h-screen overflow-y-scroll">
       <Section
         title="Perfil"
         bgColor="blue"
-        loading={loading}
-        bottom={
-          <div className="flex flex-col items-center justify-center gap-3">
-            <Avatar src={data?.data.profileImage} />
-            <h2 className="text-3xl text-brand-white">{data?.data.fullName}</h2>
-            <span className="text-sm text-gray-300 ">{data?.data.email}</span>
-          </div>
-        }
+        // loading={loading}
+        // bottom={
+        //   <div className="flex flex-col items-center justify-center gap-3">
+        //     <Avatar src={data?.data.profileImage} />
+        //     <h2 className="text-3xl text-brand-white">{data?.data.fullName}</h2>
+        //     <span className="text-sm text-gray-300 ">{data?.data.email}</span>
+        //   </div>
+        // }
       >
         <PerfilSections />
         <Button
           iconLeft={<LogOut color="red" />}
-          onClick={cerrarSesion}
+          onClick={handleLogout}
           className="my-4 w-full border-red-500 text-red-500"
           variant="secondary"
         >
