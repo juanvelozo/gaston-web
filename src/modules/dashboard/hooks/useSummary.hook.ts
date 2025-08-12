4d1fe97879c94dc6f70b29ad5c18336bf8317608
Juan Velozo <A05786@bh.com.ar>
2025-08-12 16:07:22 -0300

    [DASHBOARD] eliminamos archivos de api.ts

--- src/modules/dashboard/hooks/useSummary.hook.ts
+++ src/modules/dashboard/hooks/useSummary.hook.ts
@@ -1,7 +1,11 @@
-import { getSummary } from '../api/Summary.api';
-
+/**
+ * Hook para obtener el resumen del dashboard.
+ * Acá obtenemos el balance y el resumen de los gastos e ingresos.
+ */
 export const useSummary = () => {
-  const query = getSummary();
+  const query = () => {
+    // Lógica para obtener el resumen del dashboard
+  };
 
   return query;
 };
