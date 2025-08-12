dcc16cec3c45940541902d5f144fa5c9dc79e805
Juan Velozo <A05786@bh.com.ar>
2025-08-12 15:57:31 -0300

    [API] limpia configuración de axios

--- src/api/apiError.ts
+++ src/api/apiError.ts
@@ -1,50 +1,51 @@
-import { AxiosError } from 'axios';
-import { toast } from 'sonner';
-import { ICustomError } from '../types/proyect.model';
+// import { toast } from 'sonner';
+// import { ICustomError } from '../types/proyect.model';
 
-export function handleApiError(error: unknown): never {
-  console.log('Ocurrió un error en la petición', error);
+// export function handleApiError(error: unknown): never {
+//   console.log('Ocurrió un error en la petición', error);
 
-  if (error instanceof AxiosError) {
-    const isTimeout = error.code === 'ECONNABORTED';
-    const noResponse = !error.response;
+//   if (error) {
+//     // const isTimeout = error.code === 'ECONNABORTED';
+//     // const noResponse = !error.response;
 
-    if (isTimeout) {
-      toast.error('Ocurrió un error', {
-        description: ERROR_MESSAGES.TIMEOUT,
-      });
-    } else if (noResponse) {
-      toast.error('Ocurrió un error', {
-        description: ERROR_MESSAGES.NO_RESPONSE,
-      });
-    } else {
-      const data = error.response?.data as ICustomError;
+//     // if (isTimeout) {
+//     //   toast.error('Ocurrió un error', {
+//     //     description: ERROR_MESSAGES.TIMEOUT,
+//     //   });
+//     // } else if (noResponse) {
+//     //   toast.error('Ocurrió un error', {
+//     //     description: ERROR_MESSAGES.NO_RESPONSE,
+//     //   });
+//     // }
+//     const data = error.response?.data as ICustomError;
 
-      if (data?.message && Array.isArray(data.message)) {
-        data.message.forEach((m) =>
-          toast.error('Ocurrió un error', {
-            description: m,
-          })
-        );
-      } else {
-        toast.error('Ocurrió un error', {
-          description: ERROR_MESSAGES.INVALID_FORMAT,
-        });
-      }
-    }
-  } else {
-    toast.error('Ocurrió un error', {
-      description: ERROR_MESSAGES.UNKNOWN,
-    });
-  }
+//     if (data?.message && Array.isArray(data.message)) {
+//       data.message.forEach((m) =>
+//         toast.error('Ocurrió un error', {
+//           description: m,
+//         })
+//       );
+//     } else {
+//       toast.error('Ocurrió un error', {
+//         description: ERROR_MESSAGES.INVALID_FORMAT,
+//       });
+//     }
+//   //   else {
+//   //   }
+//   // } else {
+//   //   toast.error('Ocurrió un error', {
+//   //     description: ERROR_MESSAGES.UNKNOWN,
+//   //   });
+//   // }
 
-  throw error;
-}
+//   throw error;
+// }
 
-const ERROR_MESSAGES = {
-  TIMEOUT: 'El servidor tardó demasiado en responder. Intentalo de nuevo más tarde.',
-  NO_RESPONSE:
-    'No se pudo conectar con el servidor. Es posible que esté fuera de servicio o que haya problemas con la base de datos.',
-  INVALID_FORMAT: 'Error inesperado en la respuesta del servidor.',
-  UNKNOWN: 'Se produjo un error desconocido.',
-};
+// const ERROR_MESSAGES = {
+//   TIMEOUT: 'El servidor tardó demasiado en responder. Intentalo de nuevo más tarde.',
+//   NO_RESPONSE:
+//     'No se pudo conectar con el servidor. Es posible que esté fuera de servicio o que haya problemas con la base de datos.',
+//   INVALID_FORMAT: 'Error inesperado en la respuesta del servidor.',
+//   UNKNOWN: 'Se produjo un error desconocido.',
+// };
+export const algo = '1';
