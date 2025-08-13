23fc0fe259b9df1458098bbb3493dc66737dbc16
Juan Velozo <A05786@bh.com.ar>
2025-08-13 18:10:36 -0300

    [DASHBOARD] mensajes para mati

--- src/modules/dashboard/pages/Dashboard.tsx
+++ src/modules/dashboard/pages/Dashboard.tsx
@@ -1,3 +1,4 @@
+import { ArrowDownRight, ArrowUpRight, CoinsSwap } from 'iconoir-react';
 import CurvedLoop from '../../../components/animated/CurvedLoop/CurvedLoop.component';
 import OdometerText from '../../../components/animated/odometer/Odometer.component';
 import SpotlightCard from '../../../components/animated/SpotlightCard/SpotlightCard.component';
@@ -36,7 +37,12 @@ const DashboardPages = (): React.JSX.Element => {
             row-span-1 md:row-start-2 md:row-end-3
           "
         >
-          <h2 className="text-lg font-bold">3</h2>
+          <h2 className="text-lg font-bold text-brand-white">
+            Esta va a ser la card de Ver gastos, el icono{' '}
+            <ArrowUpRight /> grande arriba a la derecha y abajo un gráfico que representa el envío
+            de tokens (algo así como estadísticas de cuanto estuviste ganando o recibiendo.), estos gráficos en
+            responsive no se ven. Tiene que ser todo monocromático en color blanco.{' '}
+          </h2>
         </SpotlightCard>
 
         <SpotlightCard
@@ -46,7 +52,12 @@ const DashboardPages = (): React.JSX.Element => {
             row-span-1 md:row-start-2 md:row-end-3
           "
         >
-          <h2 className="text-lg font-bold">4</h2>
+          <h2 className="text-lg font-bold text-brand-white">
+            Esta va a ser la card de Ver ingresos, el icono{' '}
+            <ArrowDownRight /> grande arriba a la derecha y abajo un gráfico que representa el envío
+            de tokens (algo así como estadísticas de cuanto estuviste perdiendo o gastando.), estos gráficos en
+            responsive no se ven. Tiene que ser todo monocromático en color blanco.
+          </h2>
         </SpotlightCard>
 
         <SpotlightCard
@@ -56,7 +67,7 @@ const DashboardPages = (): React.JSX.Element => {
             row-span-1 md:row-start-3 md:row-end-4
           "
         >
-          <h2 className="text-lg font-bold">5</h2>
+          <h2 className="text-lg font-bold text-brand-white">Esta card puede ser para varias acciones, capaz para ir a las opciones de seguridad o para ver tu wallet. Fijate porfa que queda mejor.</h2>
         </SpotlightCard>
 
         <SpotlightCard
@@ -66,7 +77,7 @@ const DashboardPages = (): React.JSX.Element => {
             row-span-1 md:row-start-3 md:row-end-4
           "
         >
-          <h2 className="text-lg font-bold">6</h2>
+          <h2 className="text-lg font-bold text-brand-white">Esta es la card de "Comprar y vender" Tiene que tener el ícono de <CoinsSwap/> a la derecha (ocupar todo el alto). Acá vamos a convertir las cripto de una currency a otras. En NFID se llama Swap, así que busca sinónimos de esta palabra para mejorar el UX Writing</h2>
         </SpotlightCard>
       </div>
     </div>
